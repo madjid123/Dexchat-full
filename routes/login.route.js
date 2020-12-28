ebf9806b1e5a646aad850b1264c55a04a050b7e0
madjid123 <dexmadjid@gmail.com>
2020-12-29 00:36:00 +0100

    added login logic to the APi

--- routes/login.route.js
+++ routes/login.route.js
@@ -1,9 +1,22 @@
 
 
 const app = require('express').Router();
-
-app.get('/login', (req, res, next) => {
-    const { name, email, password } = req.body
+const sha256 = require('js-sha256')
+const User = require('../model/User.model')
+app.post('/login', (req, res, next) => {
+    if (!req.body) res.json("email and password must be provided")
+    var { email, password } = req.body
+    password = sha256(password)
+    User.findOne({ email, password }, (err, result) => {
+        if (err) console.log(err)
+        if (result) {
+            if (email == result.email && password == result.password) {
+                res.json({ err: false, msg: "Loged in successfully", name: result.name })
+            }
+        } else {
+            res.json({ err: true, msg: "there is no  such user matching this information" })
+        }
+    })
     res.json("dklfj")
 })
 module.exports = app
