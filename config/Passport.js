d7b21746d4ff2faef600316b046e3f4d243dcd40
madjid123 <dexmadjid@gmail.com>
2021-01-03 22:22:29 +0100

    fixing passport js

--- config/Passport.js
+++ config/Passport.js
@@ -1,22 +1,50 @@
-
-var passport = require('passport');
+const mongoose = require('mongoose')
 var keys = require('./keys')
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var User = require("../model/User.model")
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.google.GOOGLE_CLIENT_ID,
-            clientSecret: keys.google.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://fkrdm.com:5000/auth/google/redirect"
-        }, (accessToken, refreshToken, profile, done) => {
-            // passport callback function
-            //check if user already exists in our db with the given profile ID
-            console.log(profile)
-        })
-);
-module.exports = passport
+
+module.exports = function (passport) {
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: keys.google.GOOGLE_CLIENT_ID,
+                clientSecret: keys.google.GOOGLE_CLIENT_SECRET,
+                callbackURL: "http://fkrdm.com:5000/auth/google/callback"
+            }, async (accessToken, refreshToken, profile, done) => {
+                // passport callback function
+                //check if user already exists in our db with the given profile ID
+                console.log(accessToken, refreshToken)
+                const newUser = {
+                    googleId: profile.id,
+                    name: profile.emails[0].value.split("@")[0],
+                    email: profile.emails[0].value
+                }
+
+                try {
+                    let user = await User.findOne({ googleId: profile.id })
+
+                    if (user) {
+                        done(null, user)
+                    } else {
+                        user = await User.create(newUser)
+                        done(null, user)
+                    }
+                } catch (err) {
+                    console.error(err)
+                }
+
+
+            }));
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => done(err, user))
+    })
+}  
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var keys = require('./config/keys')
 const fs = require('fs')
 
 const mongoose = require('mongoose')
-const passport = require('./config/Passport')
+const passport = require('passport')
+require('./config/Passport')(passport)
+
 const expressSession = require('express-session')
 
 
@@ -14,6 +16,7 @@ const expressSession = require('express-session')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(passport, () => { })
 
 mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, skipCache: true }, (err) => {
     if (err) console.log("mongoose ERR : ", err.message)
@@ -22,11 +25,16 @@ mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology
 
 })
 
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(require('cors')({
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST'],
     credentials: true // enable set cookie
 }))
+
 
 app.use('/', require('./routes/register.route'))
 // fs.readdir('./routes', (err, files) => {
@@ -38,21 +46,32 @@ app.use('/', require('./routes/register.route'))
 // })
 
 app.get('/', (req, res) => {
-    console.log(req.profile)
+    console.log(req.session)
+    console.log(req.user)
     if (req.user) console.log(req.user)
     res.json("Welcome " + (req.session.user === undefined) ? "" : `${req.session.user}`)
 })
 app.get('/auth/google/logout', (req, res) => {
     console.log(req.user)
 
-    req.logOut()
-    res.redirect("https://accounts.google.com/logout")
+    req.session.destroy(function (e) {
+        req.logOut()
+        req.logout();
+        res.redirect('/');
+    });
+
 })
 
 app.get('/loggedin', (req, res, next) => {
 
-    if (req.session.user)
-        res.json({ name: req.session.user })
+    if (req.session) {
+        let name = ""
+        if (req.session.user) name = req.session.user
+        else {
+            User.find
+        }
+        res.json({ name: name })
+    }
     else {
         res.json({ err: true, msg: "Not logged in" })
     }
@@ -60,7 +79,7 @@ app.get('/loggedin', (req, res, next) => {
 app.get("/auth/google", passport.authenticate("google", {
     scope: ["profile", "email"]
 }), (req, res) => {
-    console.log(req.body)
+
 });
 
 app.get("/auth/google/redirect", (req, res) => {
@@ -74,6 +93,7 @@ app.get("/failedGoogleLogin", (req, res) => {
 app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/failedGoogleLogin' }),
     function (req, res) {
+
         res.redirect('/');
     });
 const Router = express.Router()
