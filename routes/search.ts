4163046359500ba575d10e3d0d24422d38fbf6d0
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-15 21:55:50 +0100

    rename to joinRoom

--- routes/search.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { isAuth } from "./middlewares"
-import { SearchForUserFunc } from "../controllers/search"
-const router = Router();
-router.get("/search", isAuth, SearchForUserFunc
-)
-module.exports = router
