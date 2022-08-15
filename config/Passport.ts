f20f724fa2cd29238a2a035857be8f53a0ab7cc4
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-15 21:51:28 +0100

    implement PassportUserType and using async syntax

--- config/Passport.ts
+++ config/Passport.ts
@@ -7,11 +7,11 @@ import sha256 from "js-sha256";
 import passport from "passport";
 
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
-
+export interface PassportUserType {
+  _id: mongoose.Types.ObjectId,
+  username: string,
+  email: string
+}
 passport.use(
   new GoogleStrategy.Strategy(
     {
@@ -20,8 +20,6 @@ passport.use(
       callbackURL: "http://localhost:5000/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
-      // passport callback function
-      //check if user already exists in db with the given profile ID
 
 
       try {
@@ -55,21 +53,26 @@ passport.use(
       usernameField: "username",
       passwordField: "password",
     },
-    function (username, password, done) {
-      password = sha256.sha256(password);
-      User.findOne({ username: username }, (err: mongoose.CallbackError, user: UserType) => {
-        if (err) {
-          return done(err);
-        }
+    async (username, password, done) => {
+      try {
+        password = sha256.sha256(password);
+        const user = await User.findOne({ username: username });
         if (!user) {
           return done(null, false, { message: "Incorrect email." });
         }
         if (user.password != password) {
           return done(null, false, { message: "Incorrect password." });
         }
-        return done(null, user);
-      });
+
+        const passportUser: PassportUserType = { _id: user._id, username: user.username, email: user.email, }
+        return done(null, passportUser);
+      } catch (e) {
+        let err = e as Error;
+        console.error(err.message)
+        return done(err)
+      }
     }
+
   )
 );
 passport.serializeUser((user, done) => {
