6e2a2907bdb147a481a5d2ec6d29b8c9a11959f4
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-16 22:33:49 +0100

    implement accept reject remove request functions

--- controllers/joinRoom.ts
+++ controllers/joinRoom.ts
@@ -1,11 +1,14 @@
 import { Response, Request, NextFunction } from "express"
 import mongoose from "mongoose"
+import { PassportUserType } from "../config/Passport"
 import JoinRoomRequest from "../model/JoinRoomRequest"
+import Room from "../model/Room"
 
-export const JoiningRoomRequestFunction =
+export const JoinRoomRequestFunction =
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { user_id, other_user_id } = req.params
+            const user_id = (req.user as PassportUserType)._id.toHexString()
+            const { other_user_id } = req.params
             const requestExists = await JoinRoomRequest.exists({
                 RequesterId: new mongoose.Types.ObjectId(user_id),
                 ReceiverId: new mongoose.Types.ObjectId(other_user_id),
@@ -30,5 +33,56 @@ export const JoiningRoomRequestFunction =
             console.error(err.message)
             res.status(500).send(err.message)
         }
+    }
 
+export const JoinRoomAcceptRequestFunction =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const user_id = (req.user as PassportUserType)._id.toHexString()
+            const { other_user_id } = req.params
+            const updateRes = await JoinRoomRequest.updateOne({
+                RequesterId: new mongoose.Types.ObjectId(other_user_id),
+                ReceiverId: new mongoose.Types.ObjectId(user_id),
+            }, { State: "Accepted" })
+            const newroom = new Room(
+                {
+                    members: [
+                        new mongoose.Types.ObjectId(user_id),
+                        new mongoose.Types.ObjectId(other_user_id)],
+                }
+            )
+            await newroom.save()
+            res.send("Accept Request Operation updated successfully")
+        } catch (e) {
+            const err = e as Error
+            console.error(err)
+            res.status(500).send(err)
+        }
+
+    }
+
+export const JoinRoomRejectRequestFunction =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const user_id = (req.user as PassportUserType)._id.toHexString()
+            const { other_user_id } = req.params
+        } catch (e) {
+            const err = e as Error
+            console.error(err)
+            res.status(500).send(err)
+        }
+
+    }
+
+export const JoinRoomRemoveRequestFunction =
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const user_id = (req.user as PassportUserType)._id.toHexString()
+            const { other_user_id } = req.params
+
+        } catch (e) {
+            const err = e as Error
+            console.error(err)
+            res.status(500).send(err)
+        }
     }
