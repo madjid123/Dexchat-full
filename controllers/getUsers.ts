6efde59461753070128b7a4e1007b140df247168
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-08-21 19:01:30 +0100

    add friend query param

--- controllers/getUsers.ts
+++ controllers/getUsers.ts
@@ -4,6 +4,7 @@ import User from "../model/User"
 import Room, { RoomType } from "../model/Room"
 import mongoose from "mongoose";
 import { PassportUserType } from "../config/Passport";
+
 type Users = {
     users: mongoose.Types.ObjectId[]
 }
@@ -14,29 +15,42 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
         const reqUser = req.user as PassportUserType
         const user_id = req.params.user_id
         var pattern = req.query.pattern as string
+        var friends = req.query.pattern as string
+
         if (pattern === undefined || pattern.length < 3) {
             pattern = ".*"
         }
-
         if (reqUser._id.toHexString() !== user_id) {
             throw new Error("Provided id is not the same as authenticated user id")
         }
         let rooms = await Room.find({ members: { $in: user_id } }, "members -_id")
-        let nonConnectedUsers: Users = { users: [] } as Users;
+        let connectedUsers: Users = { users: [] } as Users;
         rooms.map((room) => {
             room.members.map(member => {
                 if (member.toHexString() !== user_id)
-                    nonConnectedUsers.users.push(member)
+                    connectedUsers.users.push(member)
             })
 
         })
-        nonConnectedUsers.users.push(new mongoose.Types.ObjectId(user_id))
+        connectedUsers.users.push(new mongoose.Types.ObjectId(user_id))
+        var idFilter = {}
+        if (friends === "true") {
+            idFilter = {
+                $in: connectedUsers.users
+            }
+        } else {
+            idFilter = {
+                $nin: connectedUsers.users
+            }
+        }
+
+
         const users = await User.find({
-            _id: { $nin: nonConnectedUsers.users },
+            _id: idFilter,
             username: {
                 $in: [
-                    new RegExp(`^${pattern}`, "i"),
-                    new RegExp(`${pattern}$`, "i"),
+                    // new RegExp(`^${pattern}`, "i"),
+                    // new RegExp(`${pattern}$`, "i"),
                     new RegExp(`.*${pattern}.*`, "i"),
                 ]
             }
