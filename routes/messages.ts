f9a4a5a34f2a99b4d321dac9f2715d6067663fe2
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-14 22:53:37 +0100

    update isAuth import location

--- routes/messages.ts
+++ routes/messages.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { getMessages } from "../controllers/messages"
-import { isAuth } from "./middlewares"
+import { isAuth } from "../utils/middlewares/middlewares"
 const router = express.Router()
 const app = express()
 
