6f1f2e6fc9330621dbd0429bf4d55a8aaa21eed4
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-04 23:14:13 +0100

    Refactor RoomSendMessage function to include message timestamps

--- controllers/room.ts
+++ controllers/room.ts
@@ -1,21 +1,28 @@
 import { Response, Request, NextFunction } from "express";
 import Message, { MessageType } from "../model/Message";
 
-export const RoomSendMessage = async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const room_id = req.params.room_id
-		const message = req.body as MessageType
-		const NewMessage = new Message(message)
-		await NewMessage.validate()
-		await NewMessage.save()
-		res.json({ message: "Message was sent successfully ! " })
+export const RoomSendMessage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const room_id = req.params.room_id;
+    var message = req.body as MessageType;
+    // message.SentAt = new Date(0);
 
-
-	} catch (err) {
-		const error = err as Error
-		console.error(error)
-		res.status(400).json({ error: error.message })
-
-	}
-
-}
+    const NewMessage = new Message({
+      ...message,
+      SentAt: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    await NewMessage.validate();
+    await NewMessage.save();
+    res.json({ message: "Message was sent successfully ! " });
+  } catch (err) {
+    const error = err as Error;
+    console.error(error);
+    res.status(400).json({ error: error.message });
+  }
+};
