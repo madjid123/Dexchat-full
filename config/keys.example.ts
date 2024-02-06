7ebc5941d8ccf1121f0a6b7674ad1bf9b5904fa3
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-06 21:35:39 +0100

    Add keys.example.ts file with default values

---
+++ config/keys.example.ts
@@ -0,0 +1,10 @@
+export default {
+  mongodb: {
+    dbURI: "MONGO_DB_URL",
+  },
+  google: {
+    GOOGLE_CLIENT_ID: "GOOGLE_CLIENT_ID",
+    GOOGLE_CLIENT_SECRET: "GOOGLE_CLIENT_SECRET",
+  },
+  frontend: "FRONT_END_URL",
+};
