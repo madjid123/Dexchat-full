913c7ebb7b36a28ac46ccca045199769f00070e4
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-08 15:51:12 +0100

    Refactor User model and add password comparison method

--- model/User.ts
+++ model/User.ts
@@ -1,13 +1,16 @@
+import sha256 from "js-sha256";
 import mongoose from "mongoose";
 
 export interface UserType extends mongoose.Document {
   username: string;
-  image: string;
+  image?: string;
   email: string;
   password: string;
   googleId: string;
+  comparePassword: (password: string) => Promise<boolean>;
 }
 const userSchema = new mongoose.Schema<UserType>(
+
   {
     username: {
       type: String,
@@ -25,12 +28,21 @@ const userSchema = new mongoose.Schema<UserType>(
     },
     image: {
       type: String,
-      required: Boolean,
+
+      // required: Boolean,
     },
   },
   { timestamps: true }
 );
 
 userSchema.index({ useraname: 1 });
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  const oldpassword = sha256.sha256(password);
+
+  return this.password === oldpassword;
+};
 
 export default mongoose.model<UserType>("User", userSchema);
+
