471cbb26ae6986bf41ddc2f3ff4d1ec139bebcea
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-08 15:52:14 +0100

    Refactor user routes and add avatar and profile modification endpoints

--- routes/user.ts
+++ routes/user.ts
@@ -1,11 +1,39 @@
 import express, { Router } from "express";
-import { isAuth, isReqAuthorized } from "../utils/middlewares/middlewares"
+import { isAuth, isReqAuthorized } from "../utils/middlewares/middlewares";
 import { getRoomsHandlerFunction } from "../controllers/getRooms";
-const app = express()
-const router = Router()
+import {
+  TypedRequest,
+  modifyUserController,
+  validateModifyInputs,
+} from "../controllers/modifyUser";
+import { param, validationResult } from "express-validator";
+import {
+  avatarUploads,
+  modifyAvatarController,
+  modifyAvatarValidation,
+} from "../controllers/modifyAvatar";
+const app = express();
+const router = Router({ mergeParams: true });
 
-
-router.get("/rooms", getRoomsHandlerFunction)
-app.use("/user/:user_id", isAuth, isReqAuthorized)
-app.use("/user/:user_id", isAuth, router);
-module.exports = app
+router.get("/rooms", getRoomsHandlerFunction);
+router.post(
+  "/modify/avatar",
+  avatarUploads,
+  modifyAvatarValidation,
+  modifyAvatarController
+);
+router.post("/modify/profile", validateModifyInputs, modifyUserController);
+app.use("/user/:user_id", isAuth, isReqAuthorized, (req, res, next) => {
+  next();
+});
+app.use(
+  "/user/:user_id",
+  isAuth,
+  param("user_id", "User ID must be a valid MongoDB ID")
+    .isMongoId()
+    .withMessage("User ID must be a valid MongoDB ID")
+    .exists()
+    .withMessage("User ID must be provided"),
+  router
+);
+module.exports = app;
