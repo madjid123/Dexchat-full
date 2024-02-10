c31bb8c9e35edab59893feee647265cb6089c72f
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-10 13:44:36 +0100

    Update MongoDB URI and add prebuild script

--- config/keys.ts
+++ config/keys.ts
@@ -1,7 +1,7 @@
 export default {
   mongodb: {
-    // dbURI:"mongodb+srv://dexmad:chattest@dexmess.abdsp.mongodb.net/?retryWrites=true&w=majority",
-    dbURI: "mongodb://localhost:27017/?retryWrites=true&w=majority",
+    dbURI: "mongodb+srv://dexmad:chattest@dexmess.abdsp.mongodb.net/?retryWrites=true&w=majority",
+    // dbURI: "mongodb://localhost:27017/?retryWrites=true&w=majority",
   },
   google: {
     GOOGLE_CLIENT_ID:
