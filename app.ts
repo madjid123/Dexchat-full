c7a1b816cf8cd8d846cbf94270512ebb50a01e2a
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-11 16:18:03 +0100

    Update sameSite cookie attribute to "none"

--- app.ts
+++ app.ts
@@ -54,7 +54,7 @@ app.use(
     rolling: true,
     store: store,
     cookie: {
-      sameSite: "lax",
+      sameSite: "none",
       // sameSite: true,
       secure: process.env.NODE_ENV === "production",
       // secure: false,
