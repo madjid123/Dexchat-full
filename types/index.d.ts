14813c0f9f2c81ce27001c2836e00cdd2f2913d0
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-15 21:09:40 +0100

    Add user property to Express Request interface

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,13 @@
-import { Express, Request } from "express";
+// import { Express, Request } from "express";
 // import { Multer, File } from "multer";
 declare namespace Express {
-    export interface Request {
-        file?: Express.Multer.File; // Replace 'any' with the actual type if known
-    }
+  export interface Request {
+    file?: Express.Multer.File; // Replace 'any' with the actual type if known
+    user?: {
+      _id: string;
+      username: string;
+      email: string;
+      image?: string;
+    };
+  }
 }
