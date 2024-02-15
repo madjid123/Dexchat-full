5e35f317b486395aaf6b151b077f56723f9c2b91
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-15 21:08:55 +0100

    Refactor modifyUser controller and remove unused imports

--- controllers/modifyUser.ts
+++ controllers/modifyUser.ts
@@ -1,17 +1,7 @@
-import {
-  ValidationError,
-  body,
-  checkSchema,
-  param,
-  validationResult,
-} from "express-validator";
-import { NextFunction, Request, Response } from "express";
+import { checkSchema, validationResult } from "express-validator";
+import { Request, Response } from "express";
 import User from "../model/User";
-import multer from "multer";
-import fs from "fs";
-import path from "path";
 import { sha256 } from "js-sha256";
-import { SessionData } from "express-session";
 
 export interface TypedRequest extends Request {
   body: {
@@ -21,25 +11,7 @@ export interface TypedRequest extends Request {
     newpassword?: string;
   };
 }
-// export const validateModifyInputs = [
-//   body("username", "Username length must be a string of at least 3 characters")
-//     .isString()
-//     .withMessage("Username must be a string")
-//     .isLength({ min: 3 })
-//     .withMessage("Username must be at least 3 characters long"),
-//   body("oldpassword", "Password be a string of at least 6 characters ")
-//     .isString()
-//     .withMessage("Password must be a string")
-//     .isLength({ min: 6 })
-//     .withMessage("Password must be at least 6 characters long")
-//     .exists(),
-//   body("newpassword", "New Password be a string of at least 6 characters ")
-//     .isString()
-//     .withMessage("New Password must be a string")
-//     .isLength({ min: 6 })
-//     .withMessage("New Password must be at least 6 characters long")
-//     .optional(),
-// ];
+
 export const validateModifyInputs = checkSchema({
   username: {
     isString: true,
@@ -65,8 +37,6 @@ export const validateModifyInputs = checkSchema({
     isLength: { options: { min: 6 } },
     optional: true,
   },
-  //     .withMessage("Password must be at least 6 characters long")
-  //     .exists(),
 });
 export const modifyUserController = async (
   req: TypedRequest,
@@ -84,35 +54,22 @@ export const modifyUserController = async (
       });
     }
     const user = await User.findById(req.params.user_id);
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ errors: ["User not found"] });
     let changedUsername = false;
     changedUsername = user.username !== username;
     user.username = username;
 
     if (!(await user.comparePassword(oldpassword!))) {
-      return res.status(400).json({ message: "Invalid password" });
+      return res.status(400).json({ errors: ["Invalid password"] });
     }
     user.email = email ? email : user.email;
     user.password = newpassword ? sha256(newpassword) : user.password;
     await user.save();
-    const sessionStore = req.sessionStore;
-    if (sessionStore.clear && newpassword !== undefined) {
-      req.session.regenerate((err) => {
-        if (err) throw new Error(err);
-        (req.session as any).user = { username, email, _id: user._id };
-        req.session.save((err) => {
-          if (err) throw new Error(err);
-        });
-      })
-    }
-    if (changedUsername && sessionStore.all !== undefined) {
-      // await req.sessionStore.destroy(req.sessionID);
-
-    }
     return res.json({ username, message: "Your profile was modified" });
   } catch (e) {
     const error = e as Error;
+    const errs = [error.message];
     console.error(error);
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ errors: errs });
   }
 };
