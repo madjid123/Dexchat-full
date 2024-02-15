505982e63bb74ad19965fe588456aae54dea9671
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2024-02-15 21:08:06 +0100

    Add express-validator validationResult and handle validation errors

--- controllers/modifyAvatar.ts
+++ controllers/modifyAvatar.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import User from "../model/User";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 import fs from "fs";
 import path from "path";
 import { sha256 } from "js-sha256";
@@ -16,67 +16,76 @@ if (process.env.NODE_ENV === "production") {
 const uploads = multer({
   // preservePath: true,
   storage: multer.memoryStorage(),
-
 });
 export const avatarUploads = uploads.single("avatar");
-export const modifyAvatarValidation =
-  body("avatar", "Avatar must be an image of type jpg,png,svg,jpeg,svg").custom(
-    (value, { req }) => {
-      if (!req.file) {
-        throw new Error("No file uploaded");
-        return false;
-      }
-      const mimetypes = [
-        "image/png",
-        "image/jpeg",
-        "image/jpg",
-        "image/webp",
-        "image/svg",
-        "image/svg+xml",
-      ];
-      if (!mimetypes.includes(req.file.mimetype)) {
-        throw new Error("File type not supported");
-        return false;
-      }
-      return true;
-    }
-  );
+export const modifyAvatarValidation = body(
+  "avatar",
+  "Avatar must be an image of type jpg,png,svg,jpeg,svg"
+).custom((value, { req }) => {
+  if (!req.file) {
+    throw new Error("No file uploaded");
+    return false;
+  }
+  const mimetypes = [
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+    "image/webp",
+    "image/svg",
+    "image/svg+xml",
+  ];
+  if (!mimetypes.includes(req.file.mimetype)) {
+    throw new Error("File type not supported");
+    return false;
+  }
+  return true;
+});
 
 export const modifyAvatarController = async (req: Request, res: Response) => {
   let session: mongoose.ClientSession | null = null;
   try {
+    const validationResArray = validationResult(req).array();
+
+    if (validationResArray.length > 0) {
+      return res.status(400).json({
+        errors: validationResArray.map((error) => {
+          return error.msg;
+        }),
+      });
+    }
     session = await mongoose.startSession();
     if (!req.file) return res.status(400).json({ message: "No file uploaded" });
     const user = await User.findById(req.params.user_id);
-    if (user === null) return res.status(404).json({ message: "User not found" });
+    if (user === null)
+      return res.status(404).json({ errors: ["User not found"] });
     session.startTransaction();
 
-
-    if (!user) return res.status(404).json({ message: "User not found" });
-    const filename = sha256(req.file.originalname) + path.extname(req.file.originalname);
+    const filename =
+      sha256(req.file.originalname) + path.extname(req.file.originalname);
     const imgPath = path.join(DEST_PATH, filename);
     const relativeImgPath = path.join(IMAGE_PREFIX, filename);
     const oldimgPath = user.image;
 
-    console.log(relativeImgPath)
-    console.log(path.resolve(imgPath))
+    console.log(relativeImgPath);
+    console.log(path.resolve(imgPath));
     fs.writeFileSync(path.resolve(imgPath), req.file.buffer);
     if (oldimgPath && fs.existsSync(path.resolve(oldimgPath))) {
       fs.unlinkSync(path.resolve(oldimgPath));
     }
+
     user.image = relativeImgPath;
     await user.save();
-    await session.commitTransaction()
+    await session.commitTransaction();
     return res.json({
       username: user.username,
       message: "Your profile was modified",
     });
-  }
-  catch (e) {
+  } catch (e) {
     await session?.abortTransaction(); // Use optional chaining to handle the case when 'session' is undefined
-    const err = e as Error;
-    console.error(err.message);
-    res.status(500).json({ errors: [err.message] });
+    const error = e as Error;
+    const errs = [error.message];
+    console.error(error);
+    return res.status(500).json({ errors: errs });
   } finally {
     session?.endSession();
   }
